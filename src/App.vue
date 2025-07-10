6113a5f67ccf05a7d764bd6237394b1c5f5ec86b
aryarobyy <robyaryanata12345@gmail.com>
2025-07-10 20:39:13 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <notifications></notifications>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
