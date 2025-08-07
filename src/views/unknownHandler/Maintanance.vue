3e746171ded806d030de969a9ad5513c7ce57e3b
aryarobyy <robyaryanata12345@gmail.com>
2025-08-07 22:17:13 +0700

    adding some component

---
+++ src/views/unknownHandler/Maintanance.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-yellow-50 text-yellow-800 p-6">
+    <div class="text-6xl font-bold mb-4">🚧</div>
+    <h1 class="text-3xl font-semibold mb-2">Sedang dalam tahap Pengembangan</h1>
+    <p class="text-center max-w-md mb-6">
+      Maaf atas ketidaknyamanannya. Kami sedang melakukan pengambangan pada bagian ini. Silakan kembali lagi nanti.
+    </p>
+    <MyButton text="Kembali ke Beranda" @click="$router.push('/')" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MyButton from '../../components/Button.vue';
+
+
+</script>
